71da85f333ec89bb39e597222301fb44c04562af
ShardulJoshi05 <shardulj.nmims@gmail.com>
2025-06-08 17:09:09 +0530

    feat: added bundler and react to the project

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 // //created an object
 // const heading = React.createElement('h1', { id: 'heading' }, 'Hello React');
 
@@ -13,8 +16,8 @@ const parent = React.createElement(
   'div',
   { id: 'parent' },
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'I am h1'),
-    React.createElement('h2', {}, 'I am h2'),
+    React.createElement('h1', { key: 'a' }, 'I am h1'),
+    React.createElement('h2', { key: 'b' }, 'I am h2'),
   ])
 );
 
