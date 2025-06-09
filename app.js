69c16f1690f436abab5260790f313b203eb19a7d
ShardulJoshi05 <shardulj.nmims@gmail.com>
2025-06-09 22:19:54 +0530

    feat: created react element using jsx

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// //created an object
-// const heading = React.createElement('h1', { id: 'heading' }, 'Hello React');
-
-// //created root
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// console.log(heading);
-// // rendered the object in root. this method is responsible to convert object to the relevant tag.
-// root.render(heading);
-
-// creation of nested elements using react & rendering sibling elements
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', { key: 'a' }, 'I am h1'),
-    React.createElement('h2', { key: 'b' }, 'I am h2'),
-  ])
-);
-
+// creating header using jsx. jsx is html like syntax. jsx is used to create react element.
+const jsx = <h1>Jsx heading</h1>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
+root.render(jsx);
