6af0eb9d10078f2d5d7c9a3c36a9a4ed2518a6ed
ShardulJoshi05 <shardulj.nmims@gmail.com>
2025-06-14 11:53:34 +0530

    feat: created card and body component

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Applayout = () => {
   return (
     <>
       <HeaderComponent />
+      <BodyComponent />
     </>
   );
 };
@@ -24,6 +25,38 @@ const HeaderComponent = () => {
             <li>Cart</li>
           </ul>
         </div>
+      </div>
+    </>
+  );
+};
+
+const BodyComponent = () => {
+  return (
+    <>
+      <div className='body'>
+        <div className='search'>
+          <h3>Search</h3>
+        </div>
+        <div className='res-container'>
+          <RestaurantCard />
+        </div>
+      </div>
+    </>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <>
+      <div className='res-card'>
+        <img
+          className='res-logo'
+          src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2025/6/6/a380b487-e336-41fa-966a-2b1490e475c9_49f5550e-15a1-42cc-96f9-46e416891bff.jpg'
+        />
+        <h3>Meghana foods</h3>
+        <h4>Biryani, North Indian</h4>
+        <h4>4.4 stars</h4>
+        <h4>38 minutes</h4>
       </div>
     </>
   );
